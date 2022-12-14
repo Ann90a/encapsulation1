a2815d7ab82798bb790721348002c07f5f07c3da
Ann90a <abrosimova222@yandex.ru>
2022-12-14 18:42:35 +0300

    инкапсуляция задание 1 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+    // задание 1
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int currentYear = 2022;
+        Human maxim = new Human(currentYear - 35, "Максим", "Минск", "бренд-менеджер");
+        Human anya = new Human(currentYear - 29, "Аня", "Москва", "методист образовательных программ");
+        Human katya = new Human(currentYear - 28, "Катя", "Калининград", "продакт менеджер");
+        Human artem = new Human(currentYear - 27, "Артем", "Москва", "директор по рпзвитию бинеса");
+        Human vladimir = new Human(currentYear - 21, "Владимир", "Казань", null);
+
+        printInfo(maxim);
+        printInfo(anya);
+        printInfo(katya);
+        printInfo(artem);
+        printInfo(vladimir);
     }
+
+    private static void printInfo(Human human) {
+        System.out.println("Привет, меня зовут " + human.name + ". Я из города " + human.getTown() + ". Я родился в " + human.getYearOfBirth() + " году. Будем знакомы!");
+    }
+
 }
